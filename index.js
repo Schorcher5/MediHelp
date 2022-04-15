842600c021271a649bf3a8ca84c29458c3f838dc
Joaquin Cisneros <2014joaquincisneros@gmail.com>
2022-04-15 11:41:27 -0700

    Added the bootstrap template for index.ejs. Fixed images/static middle. Added new methods to handle home requests

--- index.js
+++ index.js
@@ -6,8 +6,31 @@ const app = express();
 const hidden = require("./hidden");
 const mongoDBURL = hidden.mongoDBURL;
 
+// Connects to the mongodb cloud database using a hidden url.
 mongoose.connect(mongoDBURL, {useNewUrlParser:true, useUnifiedTopology: true})
 	.then(result => app.listen(3000, () => console.log("Server is running")))
 	.catch(err => console.log(err));
 
+// Sets up the view engine which in this case is the ejs or embedded javascipt engine
 app.set("view engine","ejs");
+
+// Some middleware to handle the file location and data collection
+app.use(express.static("public"));
+app.use(express.urlencoded({extended:true}));
+
+// To redirect / request to the home page
+
+app.get("/", (req,res) =>{
+	res.redirect("/home");
+});
+
+app.get("/home", (req,res) => {
+	res.render("index", {title: 'Home'});
+});
+
+
+
+//If user fails to enter a real page address, then the request falls to this method where a 404 error is produced
+app.use((req,res) => {
+	res.status(404).render("404",{title:"404"});
+})
